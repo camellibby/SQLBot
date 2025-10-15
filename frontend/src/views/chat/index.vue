7606a9cf6c936c05aa73aa59c5323e9188b7063a
luoxinliang <camelxinliang@hotmail.com>
2025-10-16 02:36:10 +0800

    fix(chat):

--- frontend/src/views/chat/index.vue
+++ frontend/src/views/chat/index.vue
@@ -1,4 +1,7 @@
 <template>
+  {{assistantStore.assistant}}11
+  {{!assistantStore.pageEmbedded}}11
+  {{!assistantStore.type != 4}}11
   <el-popover
     v-if="assistantStore.assistant && !assistantStore.pageEmbedded && assistantStore.type != 4"
     :width="280"
